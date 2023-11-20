94db5060d603f830733c888c9614f0ca6b20dee2
CLAUDIO CONTI RIBEIRO <claudio_ribeiro2@estudante.sc.senai.br>
2023-11-20 20:24:36 -0300

    Alterado a main, adicionei a tela inicial

---
+++ src/view/Tela.java
@@ -0,0 +1,39 @@
+package src.view;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.*;
+
+
+public class Tela extends JFrame {
+    // Atributos
+    public static final String titulo = "TELA INICIALs";
+    private CardLayout cardLayout;
+    private JPanel cardPainel;
+    private TelaInicial telaInicial;
+
+    // Construtor
+    public Tela(){
+        super(titulo);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.cardLayout = new CardLayout();
+        
+        this.cardPainel = new JPanel();
+        this.cardPainel.setLayout(cardLayout);
+        add(cardPainel);
+
+        criarCards();
+    }
+
+    // Métodos
+    public void mostrar(){
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+    }
+    private void criarCards(){
+        telaInicial = new TelaInicial(this);
+        cardPainel.add(telaInicial, TelaInicial.class.getName());
+    }
+} // Fim da classe TelaPrincipal
