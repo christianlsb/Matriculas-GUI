c3b1e870cdd4f198f23a1a6705e052dacdf66514
natskoe <koehlernathy@gmail.com>
2023-11-20 21:33:03 -0300

    criação cadastro aluno, com atributos. sobrescrita dos métodos do JTable.

---
+++ src/model/CadastroAluno.java
@@ -0,0 +1,109 @@
+package src.model;
+
+import java.util.ArrayList;
+
+public class CadastroAluno {
+    private String nomeCompleto;
+    private int idadeMatricula;
+    private String email;
+    private String endereco;
+    private String cep;
+    private String telefone;
+    private String usuario;
+    private String senha;
+    private String curso;
+    private String observacoes;
+    private Boolean ativo;
+  
+    public String getNomeCompleto() {
+        return this.nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getIdadeMatricula() {
+        return this.idadeMatricula;
+    }
+
+    public void setIdadeMatricula(int idadeMatricula) {
+        this.idadeMatricula = idadeMatricula;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCurso() {
+        return this.curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getObservacoes() {
+        return this.observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Boolean isAtivo() {
+        return this.ativo;
+    }
+
+    public Boolean getAtivo() {
+        return this.ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
--- src/view/TabelaAluno.java
+++ src/view/TabelaAluno.java
@@ -1,7 +1,84 @@
 package src.view;
 
+import java.util.ArrayList;
+
 import javax.swing.table.AbstractTableModel;
+
+import src.model.CadastroAluno;
 
 public class TabelaAluno extends AbstractTableModel  {
     
+    //Lista dos cadastros de alunos.
+    private ArrayList<CadastroAluno> cadastrosAlunos = new ArrayList<CadastroAluno>();
+
+    //Array que titula as colunas da tabela:
+    private String[] colunas = new String[] {"Nome completo", "Idade", "E-mail", 
+                                            "Endereço", "CEP", "Telefone",
+                                            "Curso", "Observações", "Ativo"};
+
+    //Construtor:
+    public TabelaAluno(ArrayList<CadastroAluno> cadastrosAlunos){
+        this.cadastrosAlunos = cadastrosAlunos;
+    }
+    //Método que retorna a quantidade de linhas da tabela (JTable):
+	@Override
+	public int getRowCount() {
+		return cadastrosAlunos.size();
+	}
+    //Método que retorna a quantidade de linhas da tabela (JTable):
+	@Override
+	public int getColumnCount() {
+		return colunas.length;
+	}
+
+    //Método que retorna os nomes das colunas (O JTable itera pra passar por todas):
+	@Override
+	public String getColumnName(int indexColuna) {
+		String nomeColuna = null;
+
+		if (indexColuna >= 0 && indexColuna < colunas.length) {
+			nomeColuna = colunas[indexColuna];
+		}
+        //Se inválido retorna String vazia.
+		return nomeColuna;
+	}
+	@Override
+	public Object getValueAt(int indexLinha, int indexColuna) {
+		String value = null;
+
+		if (indexLinha >= 0 && indexLinha < cadastrosAlunos.size()) {
+			CadastroAluno cadastroAluno = cadastrosAlunos.get(indexLinha);
+
+			switch (indexColuna) {
+			case 0:
+				value = cadastroAluno.getNomeCompleto();
+				break;
+			case 1:
+				value = Integer.toString(cadastroAluno.getIdadeMatricula());
+				break;
+			case 2:
+				value = cadastroAluno.getEmail();
+				break;
+            case 3:
+                value = cadastroAluno.getEndereco();
+            case 4:
+                value = cadastroAluno.getCep();
+            case 5:
+                value = cadastroAluno.getTelefone();
+            case 6:
+                value = cadastroAluno.getCurso();
+            case 7:
+                value = cadastroAluno.getObservacoes();
+            case 8:
+                value = Boolean.toString(cadastroAluno.getAtivo());
+			default:
+				System.err.printf("[ERRO] Índice de coluna inválido: %d\n", indexColuna);
+			}
+		}
+
+		return value;
+	}
+
+
 }
+
